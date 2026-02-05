ff52148b4f38f1a30d799b2f1412f3cfdb947f4b
eugek0 <jmaster21042005@mail.ru>
2026-02-05 14:18:16 +0300

    feat(plugins): Добавлен плагин для интеграции nvim-tree и lsp

--- lua/plugins/helpers.lua
+++ lua/plugins/helpers.lua
@@ -14,6 +14,17 @@ return {
 	},
 
 	{
+		"antosha417/nvim-lsp-file-operations",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-tree.lua",
+		},
+		config = function()
+			require("lsp-file-operations").setup()
+		end,
+	},
+
+	{
 		"mg979/vim-visual-multi",
 		event = "VeryLazy",
 	},
