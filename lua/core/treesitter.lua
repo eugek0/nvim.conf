89dcd24ddc0b035405abb2e837fc924789fde551
eugek0 <jmaster21042005@mail.ru>
2026-02-08 01:12:58 +0300

    feat(tree-sitter): markdown добавлен в загрузку нативного nvim tree-sitter

--- lua/core/treesitter.lua
+++ lua/core/treesitter.lua
@@ -1,6 +1,6 @@
 -- Turn on neovim provided treesitter for .tsx and .jsx files
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
+	pattern = { "typescript", "javascript", "typescriptreact", "javascriptreact", "markdown" },
 	callback = function()
 		vim.treesitter.start()
 	end,
